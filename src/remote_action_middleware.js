c6f83de3896cf2fc434335061d064bbb7ae86ddc
Dean Radcliffe <dradcliffe@shutterstock.com>
2016-04-29 15:51:59 -0500

    Run on 3120

--- src/remote_action_middleware.js
+++ src/remote_action_middleware.js
@@ -3,6 +3,8 @@ import objectAssign from 'object-assign';
 export default socket => store => next => action => {
   if (action.meta && action.meta.remote) {
     const clientId = store.getState().get('clientId');
+    // TODO use rest object spread syntax, remove object-assign
+    // TODO use eslint to remove all semicolons and fix all styles
     socket.emit('action', objectAssign({}, action, {clientId}));
   }
   return next(action);
